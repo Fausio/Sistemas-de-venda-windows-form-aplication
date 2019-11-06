ca1f36a55bf7324ff250784da9d9963d748117a9
Fáusio Luis <fausioluis@live.com>
2019-11-06 09:49:24 +0200

    add form venda

---
+++ form_venda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_vendas_Cursos
+{
+    public partial class form_venda : Form
+    {
+        public form_venda()
+        {
+            InitializeComponent();
+        }
+    }
+}
